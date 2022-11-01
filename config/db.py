c810a1b2315f7305230dc240d8929e30ab31f5d0
Isha307 <56998032+Isha307@users.noreply.github.com>
2022-11-01 23:02:55 +0530

    final

--- config/db.py
+++ config/db.py
@@ -5,17 +5,3 @@ import certifi
 
 Client = MongoClient("mongodb+srv://test:test@cluster0.ax9ugoz.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 conn  = Client.Customer
-'''m = client.test
-try:
-	client.admin.command('ismaster')
-except ConnectionFailure:
-    print("Server not available")
-
-db = cluster["test"]
-conn = db["User"]
-post = {"_id": 0,
-	"name": "isha",
-	"email": "test@gmail.com",
-	"password": "test"}
-
-conn.insert_one(post)'''
